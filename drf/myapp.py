4667ab406e028629c675a869190b61dff11d51e5
AkanshuMittal <akanshumittal150@gmail.com>
2026-04-20 15:54:31 +0530

    Adding third party app

---
+++ drf/myapp.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+url = "http://127.0.0.1:8000/stucreate/"
+
+data = {
+    'name': 'Akanshu Mittal',
+    'roll': 2202900100019,
+    'city': 'Delhi'
+}
+
+json_data = json.dumps(data)
+r = requests.post(url= url, data = json_data)
+data = r.json()
+print(data)
