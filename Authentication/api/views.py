e3e05ec472310bed2ba89547ec1ef17442b64228
AkanshuMittal <akanshumittal150@gmail.com>
2026-04-22 17:32:45 +0530

    Understand about how SessionAuthentication will work based upon different permissions

--- Authentication/api/views.py
+++ Authentication/api/views.py
@@ -1,17 +1,21 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+#from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+from .custompermissions import MyPermission
 
 # Create your views here.
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
-    permission_classes = [IsAdminUser]
-    
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    #permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAdminUser]
+    #permission_classes = [IsAuthenticatedOrReadOnly]
+    #permission_classes = [DjangoModelPermissions]
+    #permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [MyPermission]
 
 
 
