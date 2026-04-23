259a4579d24c94027b10672c1160bcd23a76e899
AkanshuMittal <akanshumittal150@gmail.com>
2026-04-23 18:17:39 +0530

    Changing the serializers fields

--- Authentication/api/serializers.py
+++ Authentication/api/serializers.py
@@ -4,4 +4,4 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['id','name', 'roll', 'city']
+        fields = ['id','name', 'roll', 'city', 'passby']
