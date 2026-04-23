d27160fd3a7dbf9fe9aed46b8b29972abe735099
AkanshuMittal <akanshumittal150@gmail.com>
2026-04-23 18:21:04 +0530

    Understanding about how to generate the token with the help of signals

--- Authentication/api/models.py
+++ Authentication/api/models.py
@@ -5,3 +5,17 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     roll = models.IntegerField()
     city = models.CharField(max_length=100)
+    passby = models.CharField(max_length=50)
+
+
+# Generating the token with the help of signals
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+# This signal creates Auth Token for users
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
