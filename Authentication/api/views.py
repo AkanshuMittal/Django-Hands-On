c23a9fbb93df16849b2861db7bba5c0392c13a27
AkanshuMittal <akanshumittal150@gmail.com>
2026-04-30 23:45:15 +0530

    Adding simple jwt code

--- Authentication/api/views.py
+++ Authentication/api/views.py
@@ -1,29 +1,27 @@
-# from .models import Student
-# from .serializers import StudentSerializer
-# from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
 # from rest_framework.authentication import TokenAuthentication
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 # #from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 # #from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 # #from .custompermissions import MyPermission
 
 # Create your views here.
-# class StudentModelViewSet(viewsets.ModelViewSet):
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
-#     #authentication_classes = [BasicAuthentication, SessionAuthentication]
-#     #permission_classes = [IsAuthenticated]
-#     #permission_classes = [IsAdminUser]
-#     #permission_classes = [IsAuthenticatedOrReadOnly]
-#     #permission_classes = [DjangoModelPermissions]
-#     #permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-#     #permission_classes = [MyPermission]
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated] 
+#   #permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+#   #permission_classes = [MyPermission]
 
 
 ## For Filtering
-from .serializers import StudentSerializer
-from rest_framework.generics import ListAPIView
-from .models import Student
+# from .serializers import StudentSerializer
+# from rest_framework.generics import ListAPIView
+# from .models import Student
 
 # class StudentList(ListAPIView):
 #     # queryset = Student.objects.all()
@@ -39,29 +37,28 @@ from .models import Student
 #     serializer_class = StudentSerializer
 #     filterset_fields = ['city']
 
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
-from rest_framework.filters import OrderingFilter
+# from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework.filters import SearchFilter
+# from rest_framework.filters import OrderingFilter
 
-class StudentList(ListAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    filterset_fields = ['city', 'name']
+# class StudentList(ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#     filterset_fields = ['city', 'name']
 
 
-class StudentList(ListAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['city', 'name']
+# class StudentList(ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#     filter_backends = [SearchFilter]
+#     search_fields = ['city', 'name']
 
-class StudentList(ListAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['city', 'name']
-    search_fields = ['city', 'name']
-    ordering_fields = ['city', 'name']
+# class StudentList(ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+#     filterset_fields = ['city', 'name']
+#     search_fields = ['city', 'name']
+#     ordering_fields = ['city', 'name']
 
     
-
